74510a22c776ee40e92b683982f6d878d72acc3a
Pratosh B <pratosh.b@surveysparrow.com>
2024-02-13 10:25:29 +0530

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps:{
+    exclude: ["@rollup/rollup-linux-x64-gnu"],
+  }
 })
